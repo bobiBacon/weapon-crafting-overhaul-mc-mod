479eadd1c93ed76687470078bf2cc743c27da134
bob_bacon <antoinedsa.6@gmail.com>
2024-05-12 14:07:28 -0400

    LA HACHE A MAINTENANT UNE CAPACITÉ

---
+++ src/main/java/net/bobbacon/races/RacesModForMyServer.java
@@ -0,0 +1,42 @@
+package net.bobbacon.races;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
+import net.bobbacon.races.config.Default;
+import net.bobbacon.races.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import net.spell_engine.api.item.ItemConfig;
+import net.spell_engine.config.ServerConfigWrapper;
+import net.tinyconfig.ConfigManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RacesModForMyServer implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("races");
+	public static final String MOD_ID = "races";
+
+
+	public static ConfigManager<ItemConfig> itemConfig = new ConfigManager<ItemConfig>
+			("items_v7", Default.itemConfig)
+			.builder()
+			.setDirectory(MOD_ID)
+			.sanitize(true)
+			.build();
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+
+		ModItems.init();
+		ModItems.register(itemConfig.value.weapons);
+	}
+}
---
+++ src/main/java/net/bobbacon/races/RacesModForMyServerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bobbacon.races;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RacesModForMyServerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
