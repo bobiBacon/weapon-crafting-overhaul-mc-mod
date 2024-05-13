adc02d35ef94a4e4cca06de8579a2aed3536662c
bob_bacon <antoinedsa.6@gmail.com>
2024-05-13 16:07:21 -0400

    added a spell to the one handed war axe

--- src/main/java/net/bobbacon/races/item/ModItems.java
+++ src/main/java/net/bobbacon/races/item/ModItems.java
@@ -11,6 +11,9 @@ import net.minecraft.util.Identifier;
 import net.spell_engine.api.item.ItemConfig;
 import net.spell_engine.api.item.weapon.StaffItem;
 import net.spell_engine.api.item.weapon.Weapon;
+import net.spell_engine.api.spell.ExternalSpellSchools;
+import net.spell_power.api.SpellSchool;
+import net.spell_power.api.SpellSchools;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -35,7 +38,7 @@ public class ModItems  {
 
 //    public static final Item ONE_HANDED_WAR_AXE= Registry.register(Registries.ITEM, new Identifier(MOD_ID,"one_handed_war_axe"),new AxeItem(ToolMaterials.IRON,5,-2.5f,new FabricItemSettings()));
     public static final Weapon.Entry ONE_HANDED_WAR_AXE= entry("one_handed_war_axe", Weapon.CustomMaterial.matching(ToolMaterials.IRON,
-        ()-> Ingredient.ofItems(Items.STICK)),7,-2.8f,new OneHandedWarAxe(ToolMaterials.IRON,7,-2.8f,new FabricItemSettings()),new ItemConfig.Weapon(7,-2.8f));
+        ()-> Ingredient.ofItems(Items.STICK)),7,-2.8f,new OneHandedWarAxe(ToolMaterials.IRON,7,-2.8f,new FabricItemSettings(), ExternalSpellSchools.PHYSICAL_MELEE),new ItemConfig.Weapon(7,-2.8f));
     public static void init() {
     }
     public static void register(Map<String, ItemConfig.Weapon> configs) {
--- src/main/java/net/bobbacon/races/item/OneHandedWarAxe.java
+++ src/main/java/net/bobbacon/races/item/OneHandedWarAxe.java
@@ -1,15 +1,65 @@
 package net.bobbacon.races.item;
 
+import com.google.common.collect.Multimap;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.spell_engine.api.item.weapon.SpellWeaponItem;
+import net.spell_engine.api.spell.ExternalSpellSchools;
+import net.spell_engine.api.spell.Spell;
+import net.spell_power.api.SpellSchool;
+import net.spell_power.api.SpellSchools;
 
 public class OneHandedWarAxe extends SpellWeaponItem {
+    SpellSchool school = ExternalSpellSchools.PHYSICAL_MELEE;
 
     public OneHandedWarAxe(ToolMaterial material, Settings settings) {
         super(material, settings);
     }
 
-    public OneHandedWarAxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+    public OneHandedWarAxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings, SpellSchool school) {
         super(material, attackDamage, attackSpeed, settings);
+        this.school=school;
+    }
+    public SpellSchool getSchool() {
+        return school;
+    }
+    private Multimap<EntityAttribute, EntityAttributeModifier> attributes;
+
+    public void setAttributes(Multimap<EntityAttribute, EntityAttributeModifier> attributes) {
+        this.attributes = attributes;
+    }
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        if (this.attributes == null) {
+            return super.getAttributeModifiers(slot);
+        } else {
+            return slot == EquipmentSlot.MAINHAND ? this.attributes : super.getAttributeModifiers(slot);
+        }
+    }
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if (state.isOf(Blocks.SKELETON_SKULL)) {
+            return 5F;
+        } else {
+            return state.isIn(BlockTags.AXE_MINEABLE) ? 1.5F : 1.0F;
+        }
+    }
+
+    public boolean isSuitableFor(BlockState state) {
+        return state.isIn(BlockTags.LOGS);
+    }
+
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        stack.damage(3,miner,e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        return super.postMine(stack, world, state, pos, miner);
     }
 }
